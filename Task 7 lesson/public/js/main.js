d021933a8ac337356c2f5723e5cdaf13a3c5da16
Aleksandr Efremov <efremovaleksandr123@gmail.com>
2021-03-23 10:38:55 +0300

    Add files for lesson 7.
    К сожалению смог реализовать только вывод товаров в каталог и в корзину.
    Всё таки одного урока не достаточно, чтобы понять как работать с node.js. Понял только как делать get запросы. С тем, как обновлять или удалять информацию возникли вопросы.
    У меня в дальнейшем будет курс по Node.js, так что там эти вопросы будут разбираться более детально.

---
+++ Task 7 lesson/public/js/main.js
@@ -0,0 +1,129 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        search: "",
+        catalogUrl: '/catalogData.json',
+        basketgUrl: '/getBasket.json',
+        productsinbasket: [],
+        products: [],
+        filtered: [],
+        imgCatalog: 'img/Keyboard.jpg',
+        show: false,
+        value: 1,
+        getAllprice: 1,
+        allSum: 0,
+        numberOfItemsInBasket: 0,
+        visible: false
+    },
+    methods:
+    {
+        getJson(url) {
+            return fetch(url)
+                .then(result => result.json())
+                .catch(error => {
+                    console.log(error);
+                    this.visible = true;
+                })
+        },
+        postJson(url, data){
+            return fetch(url, {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data)
+            })
+                .then(result => result.json())
+                .catch(error => {
+                    // console.log(error)
+                    this.error.text = error;
+                })
+        },
+        addProduct(product) {
+            if (this.productsinbasket.length == 0) {
+                const block = document.getElementsByClassName("ifBasketEmpty")[0];
+                const buyBtn = document.getElementsByClassName("buy-btn-in-basket")[0];
+                block.classList.add('invisible');
+                buyBtn.classList.remove('invisible');
+            }
+            let match = 0;
+            this.productsinbasket.forEach(el => {
+                if (el.id_product == product.id_product) {
+                    match++;
+                };
+            });
+            if (match == 0) {
+
+                const prod = Object.assign({quantity: 1}, product);
+                this.postJson(`/api/cart`, prod)
+                    .then(data => {
+                        if(data.result === 1){
+                            this.productsinbasket.push(prod);
+                        }
+                    });
+            } else {
+                alert("товар уже в корзине");
+            }
+        },
+        searchItem(value) {
+            const regexp = new RegExp(value, 'i');
+            filtered = this.products.filter(product => regexp.test(product.product_name));
+            this.products.forEach(element => {
+                const block = document.getElementById(element.id_product);
+                if (!filtered.includes(element)) {
+
+                    block.classList.add('invisible');
+                } else {
+                    block.classList.remove('invisible');
+                }
+            });
+            const search = document.getElementsByClassName("ifNothingFound")[0];
+            if (filtered.length == 0) {
+                search.classList.remove('invisible');
+            } else {
+                search.classList.add('invisible');
+            }
+        },
+        deleteItemFromBasket(product) {
+            for (let i = 0; i < this.productsinbasket.length; i++) {
+                if (this.productsinbasket[i].id_product == product.id_product) {
+                    this.productsinbasket.splice(i, 1);
+                }
+            }
+            if (this.productsinbasket.length == 0) {
+                const block = document.getElementsByClassName("ifBasketEmpty")[0];
+                const buyBtn = document.getElementsByClassName("buy-btn-in-basket")[0];
+                block.classList.remove('invisible');
+                buyBtn.classList.add('invisible');
+            }
+            this.AllSum();
+        },
+        AllSum() {
+            this.allSum = 0;
+            this.productsinbasket.forEach(el => {
+                this.allSum += +el.quantity * +el.price;
+            });
+            //return this.allSum;
+
+
+        }
+    },
+    mounted() {
+        this.getJson(`/api/products`)
+            .then(data => {
+                for (let el of data) {
+                    this.products.push(el);
+                }
+            });
+
+        this.getJson(`/api/cart`)
+            .then(data => {
+                for (let el of data.contents) {
+                    this.productsinbasket.push(el);
+                }
+            });
+
+    }
+})
